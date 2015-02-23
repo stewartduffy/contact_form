de74826947eeda69d16fae36833133b07398ee1f
Stewart Duffy <stewart.duffy@elgin.org.uk>
2015-02-23 19:49:39 +1300

    Cleans up JS file some.

--- contactForm/contactForm.js
+++ contactForm/contactForm.js
@@ -1,51 +1,41 @@
-
 function init() {
-
-	//console.log("init");
-	var feedback = document.getElementById('feedback');
-	var name = document.getElementById('theName');
-	var email = document.getElementById('email');
-	var message = document.getElementById('message');
-	var spam = document.getElementById('formSpam');
-	var formSubmit = document.getElementById('form');
+    var feedback = document.getElementById('feedback'),
+        name = document.getElementById('theName'),
+        email = document.getElementById('email'),
+        message = document.getElementById('message'),
+        spam = document.getElementById('formSpam'),
+        formSubmit = document.getElementById('form');
 	
 	formSubmit.onsubmit = function(e){
 		if(validateForm()) return true;
 		return false;
-	}
+	};
 	
 	function validateForm() {
 		
 		feedback.style.display = "block";
 		feedback.innerHTML = "";
 		
-		if( name && !validName(name.value) || name.value == "Name" )
-		{
+		if( name && !validName(name.value) || name.value == "Name" ) {
 			name.focus();
 			feedback.innerHTML = "<p>Please provide a valid name</p>";
 			return false;
-		}
-		else if( email && !validEmail(email.value) )
-		{
+		} else if( email && !validEmail(email.value) ) {
 			email.focus();
 			feedback.innerHTML = "<p>Please provide a valid email address</p>";
 			return false;
-		}
-		else if( message && message.value.length == 0 || message.value == "Message" )
-		{
+		} else if( message && message.value.length === 0 || message.value == "Message" ) {
 			message.focus();
 			feedback.innerHTML = "<p>Please provide a message</p>";
 			return false;
-		}
-		else
-		{
-			feedback.innerHTML = "<p>Sending...</p>";
-			return true;
-		}
-	}
+		} else {
+            feedback.innerHTML = "<p>Sending...</p>";
+            return true;
+        }
+    }
 	
 	function validName(value) {
-		if(value.length < 2) return false;
+        if(value.length < 2) return false;
 		var reg = /^[A-Z][A-Za-z\. \-]*$/;
 		return reg.test(value);
 	}
